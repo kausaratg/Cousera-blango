f377660339bd76628a20a612c6dc9fa488fa1f27
Giwa Kausarat <kausaratg@gmail.com>
2022-10-22 21:02:36 +0000

    finsh Custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from blog.models import Post
+from django.utils import timezone
 # Create your views here.
 def index(request):
-  return render(request, "blog/index.html")
+  posts = Post.objects.all()
+  context = {"posts":posts}
+  return render(request, "blog/index.html", context)
